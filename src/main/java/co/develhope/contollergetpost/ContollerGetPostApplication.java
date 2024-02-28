4aef3d3110bb27705999552178d573376170aeea
Agostino <agostinoarcadi@gmail.com>
2024-02-28 12:08:51 +0100

    esercizio finito

---
+++ src/main/java/co/develhope/contollergetpost/ContollerGetPostApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.contollergetpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContollerGetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContollerGetPostApplication.class, args);
+	}
+
+}
